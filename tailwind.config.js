952680b1dc64a2634e81d8305da0d5394155bb8f
MartinToudal <63119367+MartinToudal@users.noreply.github.com>
2026-01-12 07:25:12 +0000

    Add files via upload
    rollback

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: { extend: {} },
   plugins: [],
 };
